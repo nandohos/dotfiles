1db33287e562f449fc17122ae86655b9826906e5
nandohos <49679853+nandohos@users.noreply.github.com>
2022-01-24 22:31:30 +0000

    kitty update & fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,8 @@ if status is-interactive
 	#cat ~/.cache/wal/sequences &
 	neofetch
 	starship init fish | source
-        fish_ssh_agent	
+        #fish_ssh_agent	
     #alias
 	alias nv="nvim"
+        # alias ng="/home/nando/.node_modules/lib/node_modules/@angular/cli/bin/ng.js"
 end
